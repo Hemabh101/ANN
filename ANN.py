0b7a7f0a4ff54a8b2bd732a0f029e5d065908f78
Hemabh Dwivedi <109017005+Hemabh101@users.noreply.github.com>
2024-01-13 12:16:34 +0530

    ANN.py

--- ANN.py
+++ ANN.py
@@ -2,31 +2,69 @@ import cv2
 import time
 import os
 
-# Access the webcam
+
 camera = cv2.VideoCapture(0)
 
-# Check if the camera is opened successfully
+
 if not camera.isOpened():
     print("Error opening camera")
     exit()
 
 while True:
-    # Capture frame-by-frame
+    
     ret, frame = camera.read()
 
-    # Display the resulting frame
+    
     cv2.imshow('Camera', frame)
 
-    # Wait for 'p' key press
+    
     key = cv2.waitKey(1) & 0xFF
     if key == ord('p'):
-        # Take a photo when 'p' is pressed
+        
         filename = f"photo_{time.strftime('%Y%m%d_%H%M%S')}.jpg"
         cv2.imwrite(filename, frame)
         photo_path = os.path.abspath(filename)
         print("Photo saved at:", photo_path)
-        break  # Exit the loop after taking the photo
+        break  
 
-# Release the camera and close the window
+
 camera.release()
+
+
+
+
+ref_images = [
+    ("Your photo", cv2.imread(photo_path))
+]
+
+cap = cv2.VideoCapture(0)
+
+should_detect = False  
+
+while True:
+    ret, frame = cap.read()
+
+    if should_detect:
+        for ref_image_name, ref_image in ref_images:
+            result = cv2.matchTemplate(frame, ref_image, cv2.TM_CCOEFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+            if max_val >= 0.8:
+                print("I see:", ref_image_name)
+                x, y = max_loc
+                w, h = ref_image.shape[1], ref_image.shape[0]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Camera", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+    elif key == ord("y"):
+        should_detect = True  
+
+cap.release()
+
+
 cv2.destroyAllWindows()
